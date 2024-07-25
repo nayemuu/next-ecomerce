de298bdc1359b71ab39dc45d5850a5e4272dd75f
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-07-26 02:27:27 +0600

    debounc added on searchbar

--- src/components/reuseable/Navbar/Navbar Mini Components/SearchBar/SearchBar/SearchBar.js
+++ src/components/reuseable/Navbar/Navbar Mini Components/SearchBar/SearchBar/SearchBar.js
@@ -1,9 +1,32 @@
 'use client';
+import { useState } from 'react';
 import './SearchBar.css';
 
 import { AiOutlineSearch } from 'react-icons/ai';
 
 function SearchBar({ smSearchBarStatus }) {
+  const [searchtext, setSearchText] = useState('');
+
+  console.log('searchtext = ', searchtext);
+
+  const doSearch = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const debounceHandler = (fn, delay) => {
+    let timeoutId;
+    return (event) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        fn(event);
+      }, delay);
+    };
+  };
+
+  const handleSearch = debounceHandler(doSearch, 500);
+
   return (
     <div
       className={`sm:max-w-[600px] w-full relative searchbar-container searchbar-container ${
@@ -20,6 +43,7 @@ function SearchBar({ smSearchBarStatus }) {
         type="search"
         placeholder="Search..."
         className="h-full w-full text-base py-[11px] sm:py-[16px] pl-[44px] searchbar"
+        onChange={handleSearch}
       />
     </div>
   );
